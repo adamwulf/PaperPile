2b0f02a875d814adbf0ac37403c1a5f0aefc73f6
Adam Wulf <adam.wulf@gmail.com>
2021-10-03 16:33:39 -0500

    Adding code from page collection view example as starting point

--- PaperPile/AppDelegate.swift
+++ PaperPile/AppDelegate.swift
@@ -1,16 +1,14 @@
 //
 //  AppDelegate.swift
-//  PaperPile
+//  Example
 //
-//  Created by Adam Wulf on 10/3/21.
+//  Created by Adam Wulf on 9/25/21.
 //
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -30,7 +28,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
-
--- PaperPile/SceneDelegate.swift
+++ PaperPile/SceneDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  SceneDelegate.swift
-//  PaperPile
+//  Example
 //
-//  Created by Adam Wulf on 10/3/21.
+//  Created by Adam Wulf on 9/25/21.
 //
 
 import UIKit
@@ -10,7 +10,6 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -47,6 +46,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
-
 }
-
