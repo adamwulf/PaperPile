9ca554621744758fe4023b76ea8b9bf11370814f
Adam Wulf <adam.wulf@gmail.com>
2021-10-03 16:27:36 -0500

    Initial Commit

---
+++ PaperPile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaperPile
+//
+//  Created by Adam Wulf on 10/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
